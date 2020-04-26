fd8223ea7c99e91f55c371901326a73f55b0abf0
Daiki-Kaihatsu <daichan.kaihatsu@gmail.com>
2020-04-26 16:58:39 +0900

    セクション３　イベント一覧に作成したイベントを表示させよう

--- src/component/App.js
+++ src/component/App.js
@@ -2,6 +2,8 @@ import React,{useReducer,useState} from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Event from './Event'
+
 import reducer from '../reducers'
 const App = () => {
   const [state, dispatch] = useReducer(reducer,[])
@@ -19,7 +21,6 @@ const App = () => {
     setBody('')
   }
 
-  console.log({state})
 
   return (
     <>
@@ -49,6 +50,9 @@ const App = () => {
               <th></th>
             </tr>
           </thead>
+          <tbody>
+            {state.map((event, index) => (<Event key={index} event={event} dispatch={dispatch}/>))}
+          </tbody>
         </table>
       </div>
     </>
